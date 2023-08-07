346a5921732ccedf5d16f6b9aff75de1813e66e4
João Bizzo <92961230+joaobizzo@users.noreply.github.com>
2023-08-06 23:43:06 -0300

    Update filter_text_and_send2csv.py

--- filter_text_and_send2csv.py
+++ filter_text_and_send2csv.py
@@ -30,10 +30,6 @@ with open(new_chat_txt, 'r') as f:
 # Replace the third occurrence of ':' with ';'
 modified_lines = []
 for line in chat_lines:
-    # Add a semicolon at the 20th character position
-    if len(line) >= 21:
-        line = line[:21] + ';' + line[21:]
-
     parts = line.split(':', 2)  # Split the line into 3 parts at the first two colons
     if len(parts) == 3:  # Ensure there are at least three parts
         modified_line = f"{parts[0]}:{parts[1]}:{parts[2].replace(':', ';', 1)}"  # Replace the third colon with semicolon
@@ -47,8 +43,8 @@ modified_lines = [line.replace('[', '').replace(']', '') for line in modified_li
 # Change the 11th character to a semicolon
 modified_lines = [line[:10] + ';' + line[11:] if len(line) > 10 else line for line in modified_lines]
 
-# Remove the 20th character from each line
-modified_lines = [line[:20] + line[21:] if len(line) > 20 else line for line in modified_lines]
+# Change the 20th character to a semicolon
+modified_lines = [line[:19] + ';' + line[20:] if len(line) > 19 else line for line in modified_lines]
 
 # Write the modified lines back to the new CSV file
 with open(final_chat_csv, 'w') as f:
